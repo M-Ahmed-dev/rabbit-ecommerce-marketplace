574c5f28b21481b3b502bd98624baa4653fa10e3
M-Ahmed-dev <winterfell551@gmail.com>
2025-06-23 17:59:29 +0500

    completed dynamic url appending and feature filters

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -28,7 +28,7 @@ function Navbar() {
         {/* Center nav */}
         <div className="hidden md:flex space-x-6">
           <Link
-            to="#"
+            to="/collections/all"
             className="text-gray-700 hover:text-black text-sm font-medium uppercase"
           >
             Men
